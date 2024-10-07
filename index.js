dfd3bb800ace949e0c893e624b40f3da4013aadf
francesc-pablo <francescpacopablo@gmail.com>
2024-10-07 14:04:34 +0000

    second commit

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ import cors from "cors";
 // connection to database
 await mongoose.connect(process.env.MONGO_URI);
 
+// create an express app
+const app = express();
+
 // middlewares
 app.use(express.json());
+app.use(cors());
 
 // use routes
-app.use(cors());
+
 
 // listening
 app.listen(3002, () => {
