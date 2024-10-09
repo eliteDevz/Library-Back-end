cf98b2f666886fdcb0128bfe241a3b7143e81b90
TheeDee6 <officialtheodora20@gmail.com>
2024-10-09 14:05:19 +0000

    added authors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import { authorRouter } from "./routes/author_route.js";
+import { reviewRouter } from "./routes/review_route.js";
+import { booksRouter } from "./routes/book_route.js";
 
 // connection to database
 await mongoose.connect(process.env.MONGO_URI);
@@ -15,9 +17,10 @@ app.use(cors());
 
 // use routes
 app.use(authorRouter);
+app.use(reviewRouter);
+app.use(authorRouter);
 
 // listening
 app.listen(3002, () => {
     console.log('app is listening on port 3002')
 });
-
